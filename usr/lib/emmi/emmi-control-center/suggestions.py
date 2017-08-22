919db34c0a5a77df6188c63297c05cd5f39ee948
Slackjeff <lrcjefferson@gmail.com>
2017-08-22 17:50:10 -0300

    Update suggestions.py

--- usr/lib/emmi/emmi-control-center/suggestions.py
+++ usr/lib/emmi/emmi-control-center/suggestions.py
@@ -2,10 +2,9 @@
 # -*- coding: utf-8 -*-
 
 """
- Tuquito Control Center
- Copyright (C) 2010
- Author: Mario Colque <mario@emmi.org.ar>
- Tuquito Team! - www.emmi.org.ar
+ Emmi Control Center
+ Copyright (C) 2017
+
 
  This program is free software; you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
